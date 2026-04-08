8e15ddeba671d137935421a99c9126a1d5905597
Marijn-dev <marijnruiter09@gmail.com>
2026-04-08 18:17:29 +0200

    Dynamics and ParameterGenerator imports

--- semble/__init__.py
+++ semble/__init__.py
@@ -5,4 +5,9 @@ from .trajectory_sampler import (
     TSamplerSpec,
 )
 
+from .parameter_generators import get_parameter_generator, ParameterGenerator
+
+from .dynamics import Dynamics, get_dynamics
+
 from . import dynamics, sequence_generators, initial_state, parameter_generators
+
